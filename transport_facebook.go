25476bace59489069b396b25035d7c1ad56e4c77
Matias Insaurralde <matias@insaurral.de>
2016-05-26 23:50:18 -0400

    Form

--- transport_facebook.go
+++ transport_facebook.go
@@ -10,10 +10,28 @@ type FacebookTransport struct {
   Login string
   Password string
   Browser *browser.Browser
+  Authenticated bool
 }
 
 func( t *FacebookTransport ) Prepare() {
-  t.Browser = surf.NewBrowser()
   fmt.Println("FacebookTransport, Prepare()")
+  t.Browser = surf.NewBrowser()
+  t.Authenticated = false
+
+  t.DoLogin()
+
   return
+}
+
+func( t *FacebookTransport ) DoLogin() {
+  fmt.Println("FacebookTransport, Login()")
+  err := t.Browser.Open( "https://mobile.facebook.com/")
+  if err != nil {
+    panic(err)
+  }
+  LoginForm, _ := t.Browser.Form( "mobile-login-form" )
+  fmt.Println(LoginForm)
+  LoginForm.Input( "username", "abc")
+  LoginForm.Input( "password", "123")
+  LoginForm.Submit()
 }
