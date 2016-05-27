f7f52381fe318b4060cc8ec4a370e36ecba4178e
Matias Insaurralde <matias@insaurral.de>
2016-05-27 04:19:15 -0400

    Removing some funcs

--- proxy.go
+++ proxy.go
@@ -7,12 +7,6 @@ import (
 	"errors"
 )
 
-type Request struct {
-	Method  string
-	URL     string
-	Proto   string
-	Headers map[string][]string
-}
 
 type Proxy struct {
 	Port      int
@@ -31,8 +25,8 @@ func (proxy *Proxy) Listen() {
 	transport := proxy.Transport.(FacebookTransport)
 	transport.Prepare()
 
-	http.HandleFunc("/", HandleRequest)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", transport.Handler)
+	http.ListenAndServe( ":8080", nil)
 
 	return
 }
@@ -47,32 +41,3 @@ func MarshalRequest(request *http.Request) []byte {
 	output, _ := json.Marshal(r)
 	return output
 }
-
-func HandleRequest(w http.ResponseWriter, originalRequest *http.Request) {
-	client := &http.Client{}
-	request, _ := http.NewRequest(originalRequest.Method, originalRequest.URL.String(), nil)
-
-	fmt.Println("Got", originalRequest)
-	fmt.Println("Recreated", request)
-	fmt.Println("client", client)
-
-	// fmt.Println(resp.Body, err)
-	return
-}
-
-/*
-func main() {
-  fmt.Println( "Transports Test")
-  // transport := &http.Transport{}
-  // client := &http.Client{ Transport: transport }
-
-  request, _ := http.NewRequest( "GET", "http://whatismyip.akamai.com/", nil)
-  fmt.Println(request)
-
-  jsonRequest := MarshalRequest(request)
-  fmt.Println( "JSON Request:", string(jsonRequest))
-
-  http.HandleFunc("/", HandleRequest)
-  http.ListenAndServe(":8080", nil)
-}
-*/
