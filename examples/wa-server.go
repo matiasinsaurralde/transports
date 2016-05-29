a8f9816872893cff673c6f8b73f1bfce54f3d880
Matias Insaurralde <matias@insaurral.de>
2016-05-29 04:24:10 -0400

    Implementing a basic "server"

---
+++ examples/wa-server.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+  "github.com/matiasinsaurralde/transports"
+  "github.com/joho/godotenv"
+  "net/http"
+  "io/ioutil"
+  "fmt"
+  "os"
+)
+
+func main() {
+  godotenv.Load()
+
+  fmt.Println("Transports test (Whatsapp/Yowsup)")
+
+  whatsappTransport := transports.WhatsappTransport{
+    Login: os.Getenv( "WA_LOGIN" ),
+    Password: os.Getenv( "WA_PASSWORD" ),
+    Contact: os.Getenv( "WA_CONTACT" ),
+  }
+
+  whatsappTransport.Listen( func( t *transports.WhatsappTransport ) {
+    fmt.Println("callback!", t.Messages)
+    for _, Value := range t.Messages {
+      request := t.Serializer.Deserialize([]byte(Value.Body))
+
+      client := &http.Client{}
+
+      response, _ := client.Do(request)
+
+      defer response.Body.Close()
+
+      rawBody, _ := ioutil.ReadAll( response.Body)
+
+      t.SendMessage( string(rawBody))
+      t.PurgeMessage( Value.Id)
+    }
+
+    t.Messages = make([]transports.WhatsappMessage, 0)
+  })
+}
