5c7c1ed4a35c75696dcc8582d0de260f8e939637
Matias Insaurralde <matias@insaurral.de>
2016-05-29 04:25:10 -0400

    Request unmarshalling

--- serialization.go
+++ serialization.go
@@ -3,6 +3,7 @@ package transports
 import(
   "encoding/json"
   "net/http"
+  "fmt"
 )
 
 type Request struct {
@@ -16,12 +17,24 @@ type DefaultSerializer struct {
 }
 
 func( serializer *DefaultSerializer ) Serialize( req *http.Request ) []byte {
+  fmt.Println("***_", req)
     r := Request{
       Method:  req.Method,
       URL:     req.URL.String(),
       Proto:   req.Proto,
       Headers: req.Header,
     }
+    fmt.Println("***r",r)
     output, _ := json.Marshal(r)
+    fmt.Println("***m", string(output))
     return output
+}
+
+func( serializer *DefaultSerializer ) Deserialize( Input []byte ) *http.Request {
+  r := Request{}
+
+  json.Unmarshal( Input, &r )
+  request, _ := http.NewRequest( r.Method , r.URL, nil)
+  
+  return request
 }
