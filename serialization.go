79bcc7b76c80997d43960a13965f7d014cdc385e
Matias Insaurralde <matias@insaurral.de>
2016-05-31 02:01:31 -0400

    Implementic a generic serializer and separate deserialization functions for request & response

--- serialization.go
+++ serialization.go
@@ -13,28 +13,66 @@ type Request struct {
 	Headers map[string][]string
 }
 
+type Response struct {
+  Status string
+  StatusCode int
+  Proto string
+  Headers map[string][]string
+  Body string
+}
+
 type DefaultSerializer struct {
 }
 
-func( serializer *DefaultSerializer ) Serialize( req *http.Request ) []byte {
-  fmt.Println("***_", req)
-    r := Request{
+func( serializer *DefaultSerializer ) Serialize( req interface{}, jsonOutput bool ) interface{} {
+
+  fmt.Println( "Serialize" )
+
+  var output []byte
+  var r interface{}
+
+  switch t := req.(type) {
+  case *http.Request:
+    req := req.(*http.Request)
+    r = Request{
       Method:  req.Method,
       URL:     req.URL.String(),
       Proto:   req.Proto,
       Headers: req.Header,
     }
-    fmt.Println("***r",r)
-    output, _ := json.Marshal(r)
-    fmt.Println("***m", string(output))
+  case *http.Response:
+    res := req.(*http.Response)
+    r = Response{
+      Status: res.Status,
+      StatusCode: res.StatusCode,
+      Proto: res.Proto,
+      Headers: res.Header,
+    }
+  default:
+    fmt.Println( "Unknown Type", t)
+  }
+
+  if jsonOutput {
+    output, _ = json.Marshal(r)
     return output
+  } else {
+    return r
+  }
 }
 
-func( serializer *DefaultSerializer ) Deserialize( Input []byte ) *http.Request {
+func( serializer *DefaultSerializer ) DeserializeRequest( Input []byte ) *http.Request {
   r := Request{}
 
   json.Unmarshal( Input, &r )
   request, _ := http.NewRequest( r.Method , r.URL, nil)
-  
+
   return request
+}
+
+func( serializer *DefaultSerializer ) DeserializeResponse( Input []byte ) Response {
+  r := Response{}
+
+  json.Unmarshal( Input, &r )
+
+  return r
 }
