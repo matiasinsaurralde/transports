e7f1d9cf570bebb2d6685eca7efefe340b3da7d0
Matias Insaurralde <matias@insaurral.de>
2016-06-05 00:04:12 -0400

    Requiring two or more marshalers to initialize a chain

--- marshalers/chain.go
+++ marshalers/chain.go
@@ -1,66 +1,64 @@
 package transports
 
-import("log")
+import (
+	"errors"
+	"log"
+)
+
+const ChainSingleMarshalerError string = "A chain requires two or more Marshalers."
 
 type ChainData struct {
-  marshalers []Marshaler
-  input interface{}
+	marshalers []Marshaler
+	input      interface{}
 }
 
-type Chain interface{
-  Marshal( interface{} ) ( error, interface{} )
-  process() ( error, interface{} )
+type Chain interface {
+	Marshal(interface{}) (error, interface{})
+	process() (error, interface{})
 }
 
-func NewChain( marshalers ...Marshaler) Chain {
-  log.Println("Got", marshalers)
-  data := ChainData{marshalers, nil}
-  return Chain( &data )
-  /*
-  c := Chain()
-  for i, m := range marshalers {
-    log.Println(i,m)
-  }
-  log.Println("x", c.Process() )
-  */
+func NewChain(marshalers ...Marshaler) (error, Chain) {
+	var err error
+	if len(marshalers) <= 1 {
+		err = errors.New(ChainSingleMarshalerError)
+		return err, nil
+	}
+	data := ChainData{marshalers, nil}
+	return err, Chain(&data)
 }
 
-func (s *ChainData) process() ( error, interface{} ) {
-  log.Println("Process()", s )
-  /*x := DummyMarshaler{}
-  mm := Marshaler(&x)
-  log.Println("xd",mm)*/
+func (s *ChainData) process() (error, interface{}) {
+	log.Println("Process()", s)
 
-  var output interface{}
-  // var errors bool = false
+	var output interface{}
 
-  var err error
+	var err error
 
-  for i, m := range s.marshalers {
-    
-    log.Println( "--> Chain step #", i )
-    if output == nil {
-      log.Println( "No previous output, starting chain" )
-      err, output = m.Marshal(&s.input)
-      log.Println( "First output:", output, "Error:", err )
-    } else {
-      log.Println( "Previous output", output )
-      err, output = m.Marshal(&output)
-      log.Println( "New output:", output, "Error:", err )
-    }
+	for i, m := range s.marshalers {
 
-    if err != nil {
-      break
-    }
+		log.Println("--> Chain step #", i)
+		if output == nil {
+			log.Println("No previous output, starting chain")
+			err, output = m.Marshal(&s.input)
+			log.Println("First output:", output, "Error:", err)
+		} else {
+			log.Println("Previous output", output)
+			err, output = m.Marshal(&output)
+			log.Println("New output:", output, "Error:", err)
+		}
 
-  }
+		if err != nil {
+			break
+		}
 
-  return err, output
+	}
+
+	return err, output
 }
 
-func (s *ChainData) Marshal(i interface{}) ( error, interface{} ) {
-  log.Println("Output()", s)
-  s.input = i
-  err, output := s.process()
-  return err, output
+func (s *ChainData) Marshal(i interface{}) (error, interface{}) {
+	log.Println("Output()", s)
+	s.input = i
+	err, output := s.process()
+	return err, output
 }
--- marshalers/chain_test.go
+++ marshalers/chain_test.go
@@ -1,38 +1,56 @@
 package transports_test
 
-import(
+import (
 	"github.com/matiasinsaurralde/transports/marshalers"
 	// "github.com/matiasinsaurralde/transports/marshalers/protos"
 
+	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
-	"log"
 )
 
 func init() {
 
 	log.Println("init")
 
-	url, _ := url.Parse( "http://whatismyip.akamai.com/")
+	url, _ := url.Parse("http://whatismyip.akamai.com/")
 
 	request = http.Request{
 		Method: "GET",
-		URL: url,
-		Proto: "HTTP/1.0",
+		URL:    url,
+		Proto:  "HTTP/1.0",
 	}
 }
 
-func TestChaining( t *testing.T ) {
-  chain := transports.NewChain( transports.ProtobufMarshaler{},
-                                transports.DummyMarshaler{} )
+func TestBasicChaining(t *testing.T) {
+	err, chain := transports.NewChain(transports.ProtobufMarshaler{},
+		transports.DummyMarshaler{})
 
-
-  // chain := transports.Chain()
-  // chain.process()
-  output, err := chain.Marshal( &request )
-  log.Println("output", output, "err", err)
-  // log.Println(1,chain)
+	// chain := transports.Chain()
+	// chain.process()
+	err, output := chain.Marshal(&request)
+	log.Println("err", err, "output", output)
+	// log.Println(1,chain)
 
 	return
+}
+
+func TestChainingWithSingleOrNoMarshalers(t *testing.T) {
+	err, chain := transports.NewChain(transports.ProtobufMarshaler{})
+	if err == nil || chain != nil {
+		t.Fatal("A chain should have at least two Marshalers")
+	}
+	if strings.Index(err.Error(), transports.ChainSingleMarshalerError) < 0 {
+		t.Fatal("A chain should have at least two Marshalers")
+	}
+
+	err, chain = transports.NewChain()
+	if err == nil || chain != nil {
+		t.Fatal("A chain should have at least two Marshalers")
+	}
+	if strings.Index(err.Error(), transports.ChainSingleMarshalerError) < 0 {
+		t.Fatal("A chain should have at least two Marshalers")
+	}
 }
