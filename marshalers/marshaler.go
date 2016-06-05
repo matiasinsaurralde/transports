fc2f1d5b15926c98677cbd10874c0b16ec9a0ba3
Matias Insaurralde <matias@insaurral.de>
2016-06-05 01:25:13 -0400

    Defining MarshalerUnexpectedOutput

--- marshalers/marshaler.go
+++ marshalers/marshaler.go
@@ -3,6 +3,8 @@ package transports
 const MarshalerNilTypeError string = "Marshaler can't handle a nil value."
 const MarshalerTypeNotSupportedError string = "Marshaler doesn't support the type you're using:"
 
+const MarshalerUnexpectedOutput string = "Unexpected Marshaler output"
+
 type Marshaler interface{
   Marshal(*interface{}) (error, interface{})
   Unmarshal()
