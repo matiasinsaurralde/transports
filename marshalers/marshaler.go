281c82aceca1deccc00bae027fa850dae74d6666
Matias Insaurralde <matias@insaurral.de>
2016-06-05 00:06:26 -0400

    Basic Marshaler error messages

--- marshalers/marshaler.go
+++ marshalers/marshaler.go
@@ -1,5 +1,8 @@
 package transports
 
+const MarshalerNilTypeError string = "Marshaler can't handle a nil value."
+const MarshalerTypeNotSupportedError string = "Marshaler doesn't support the type you're using:"
+
 type Marshaler interface{
   Marshal(*interface{}) (error, interface{})
   Unmarshal()
