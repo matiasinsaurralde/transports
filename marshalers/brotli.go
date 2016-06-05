21a0865962790e6381894f708767df2311c8cbca
Matias Insaurralde <matias@insaurral.de>
2016-06-05 01:24:47 -0400

    Pushing brotli

---
+++ marshalers/brotli.go
@@ -0,0 +1,36 @@
+package transports
+
+import (
+  "errors"
+  "gopkg.in/kothar/brotli-go.v0/enc"
+)
+
+type BrotliMarshaler struct {
+}
+
+func (marshaler BrotliMarshaler) Marshal(i *interface{}) (error, interface{}) {
+  var err error
+
+  if i == nil {
+    err = errors.New( MarshalerNilTypeError )
+    return err, nil
+  }
+
+  switch (*i).(type) {
+  case []byte:
+  default:
+    err = errors.New( MarshalerTypeNotSupportedError )
+		return err, nil
+  }
+
+  var inputBuf []byte
+  inputBuf = (*i).([]byte)
+
+  buf, err := enc.CompressBuffer( nil, inputBuf, make( []byte, 0 ) )
+
+	return err, buf
+}
+
+func (marshaler BrotliMarshaler) Unmarshal() {
+	return
+}
---
+++ marshalers/brotli_test.go
@@ -0,0 +1,65 @@
+package transports_test
+
+import(
+	"github.com/matiasinsaurralde/transports/marshalers"
+  "gopkg.in/kothar/brotli-go.v0/dec"
+  "strings"
+  "bytes"
+  "testing"
+)
+
+func TestBrotliMarshalChain ( t *testing.T ) {
+
+  _, chain := transports.NewChain(
+    transports.ProtobufMarshaler{},
+    transports.DummyMarshaler{},
+  )
+  _, output := chain.Marshal(&request)
+
+  protobufOutput := output.([]byte)
+
+  _, chain = transports.NewChain(
+    transports.ProtobufMarshaler{},
+    transports.BrotliMarshaler{},
+  )
+
+  _, output = chain.Marshal( &request)
+
+  compressedOutput := output.([]byte)
+
+  decompressedProtobuf, _ := dec.DecompressBuffer(compressedOutput, make([]byte, 0))
+
+  if !bytes.Equal( protobufOutput, decompressedProtobuf ) {
+    t.Fatal(transports.MarshalerUnexpectedOutput)
+  }
+
+}
+
+func TestBrotliUnsupportedType( t *testing.T ) {
+	var marshaler transports.Marshaler
+	marshaler = transports.BrotliMarshaler{}
+
+	var v UnknownType
+	v = UnknownType{"Value"}
+
+	var i interface{}
+	i = v
+
+	err, _ := marshaler.Marshal(&i)
+
+	exists := strings.Index(err.Error(), transports.MarshalerTypeNotSupportedError)
+
+	if exists < 0 {
+		t.Fatal("Unsupported type doesn't break the Brotli marshaler")
+	}
+}
+
+func TestBrotliNilInput( t *testing.T ) {
+	var marshaler transports.Marshaler
+	marshaler = transports.BrotliMarshaler{}
+	err, _ := marshaler.Marshal(nil)
+
+	if strings.Index( err.Error(), transports.MarshalerNilTypeError ) < 0 {
+		t.Fatal("Nil type doesn't break the Brotli marshaler")
+	}
+}
