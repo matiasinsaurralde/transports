9f93f881d0c03aefaa28597a278a75ff1829f340
Matias Insaurralde <matias@insaurral.de>
2016-06-05 02:41:19 -0400

    Implementing Brotli Unmarshal()

--- marshalers/brotli.go
+++ marshalers/brotli.go
@@ -2,6 +2,7 @@ package transports
 
 import (
 	"errors"
+	"gopkg.in/kothar/brotli-go.v0/dec"
 	"gopkg.in/kothar/brotli-go.v0/enc"
 )
 
@@ -31,6 +32,26 @@ func (marshaler BrotliMarshaler) Marshal(i *interface{}) (error, interface{}) {
 	return err, buf
 }
 
-func (marshaler BrotliMarshaler) Unmarshal() {
-	return
+func (marshaler BrotliMarshaler) Unmarshal(i *interface{}) (error, interface{}) {
+	var err error
+
+	if i == nil {
+		err = errors.New(MarshalerNilTypeError)
+		return err, nil
+	}
+
+	switch (*i).(type) {
+	case []byte:
+	default:
+		err = errors.New(MarshalerTypeNotSupportedError)
+		return err, nil
+	}
+
+
+	var inputBuf []byte
+	inputBuf = (*i).([]byte)
+
+	buf, err := dec.DecompressBuffer( inputBuf, make([]byte, 0))
+
+	return err, buf
 }
